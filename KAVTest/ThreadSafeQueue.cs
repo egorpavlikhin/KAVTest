aa07b883ddf66e9681ed8cd4553e4a4bbc5a726c
Egor Pavlikhin <egor.pavlikhin@gmail.com>
2017-05-07 23:17:50 +0300

    Thread safe queue.

---
+++ KAVTest/ThreadSafeQueue.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace KAVTest
+{
+    public class ThreadSafeQueue<T>
+    {
+        private readonly SemaphoreSlim popSemaphore = new SemaphoreSlim(0, 1);
+
+        private readonly SemaphoreSlim readWriteSemaphore = new SemaphoreSlim(1, 1);
+
+        private Queue<T> queue = new Queue<T>();
+
+        public void Push(T item)
+        {
+            readWriteSemaphore.Wait();
+            try
+            {
+                queue.Enqueue(item);
+
+                if (queue.Count == 1) // if it's the first item release the pop handle
+                {
+                    popSemaphore.Release();
+                }
+            }
+            finally
+            {
+                readWriteSemaphore.Release();
+            }
+        }
+
+        public T Pop()
+        {
+            readWriteSemaphore.Wait();
+            try
+            {
+                if (queue.Count == 0) // wait for new items
+                {
+                    readWriteSemaphore.Release(); // allow items to be added
+                    popSemaphore.Wait(); // TODO: specify a timeout
+                }
+
+                if (queue.Count == 1 && popSemaphore.CurrentCount == 1) // if queue is empty reset semaphore to 0
+                    popSemaphore.Wait(); // TODO: specify a timeout
+
+                return queue.Dequeue();
+            }
+            finally
+            {
+                if (readWriteSemaphore.CurrentCount == 0)
+                    readWriteSemaphore.Release();
+            }
+        }
+    }
+}
---
+++ KAVTest/ThreadSafeQueueTests.cs
@@ -0,0 +1,56 @@
+﻿using System.Threading.Tasks;
+using Xunit;
+
+namespace KAVTest
+{
+    public class ThreadSafeQueueTests
+    {
+        [Fact]
+        public async void IsThreadSafe()
+        {
+            var queue = new ThreadSafeQueue<decimal>();
+
+            decimal result = 0;
+            var popTask = Task.Factory.StartNew(() => { result = queue.Pop(); });
+            await Task.Delay(500);
+            Assert.Equal(0, result);
+            await Task.Factory.StartNew(() => { queue.Push(10M); });
+            await popTask;
+            Assert.Equal(10, result);
+        }
+
+        [Fact]
+        public async void CanPopAndPushLastElement()
+        {
+            var queue = new ThreadSafeQueue<decimal>();
+
+            decimal result = 0;
+            var popTask = Task.Factory.StartNew(() => { result = queue.Pop(); });
+            await Task.Delay(500);
+            Assert.Equal(0, result);
+            await Task.Factory.StartNew(() => { queue.Push(10M); });
+            await Task.Factory.StartNew(() => { queue.Push(30M); });
+            await Task.Factory.StartNew(() => { queue.Push(50M); });
+            await popTask;
+            Assert.Equal(10, result);
+
+            popTask = Task.Factory.StartNew(() => { result = queue.Pop(); });
+            await popTask;
+            Assert.Equal(30, result);
+
+            popTask = Task.Factory.StartNew(() => { result = queue.Pop(); });
+            await popTask;
+            Assert.Equal(50, result);
+
+            result = 0M;
+
+            popTask = Task.Factory.StartNew(() => { result = queue.Pop(); });
+            await Task.Delay(500);
+            Assert.Equal(0M, result);
+
+            await Task.Factory.StartNew(() => { queue.Push(55M); });
+            await popTask;
+            Assert.Equal(55, result);
+        }
+    }
+}
